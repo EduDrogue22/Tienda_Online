e75e1ca5850db5cd78c360035482d80c715062d3
EduDrogue22 <edu.droguett01@gmail.com>
2024-06-02 17:42:12 -0400

    Cambio con Gunicorn

--- backendtienda/backendtienda/settings.py
+++ backendtienda/backendtienda/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -148,3 +148,5 @@ CORS_ALLOWED_ORIGINS = [
     config('FRONTEND'),
     config('FRONTENDLOCAL'),
 ]
+
+ALLOWED_HOSTS = ['*']
